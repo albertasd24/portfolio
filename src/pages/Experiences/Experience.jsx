15bc87a1f0fe62de90c056779b968457b0775356
albertasd24 <136749891+albertasd24@users.noreply.github.com>
2024-10-29 15:37:56 -0500

    Avance

--- src/pages/Experiences/Experience.jsx
+++ src/pages/Experiences/Experience.jsx
@@ -5,9 +5,10 @@ const Experience = () => {
         <section className="section-experience">
             <h1>Experiencia</h1>
             <div className="container-cards-experience">
-                <CardExperience experience={{ date: "June 2024 - June 2025", nameBussines: "Google", roleBussines: "FullStack Enginer", tags: ["HTML", "CSS", "JavaScript"] }} />
-                <CardExperience experience={{ date: "June 2024 - June 2025", nameBussines: "Grupo ASD", roleBussines: "FullStack Enginer", tags: ["Frontend"] }} />
-                <CardExperience experience={{ date: "June 2024 - June 2025", nameBussines: "Microsoft", roleBussines: "FullStack Enginer", tags: ["Backend","FullStack"] }} />
+                <CardExperience experience={{ date: "Junio 202 - Actualmente", nameBussines: "Grupo ASD", roleBussines: "Backend Developer", tags: ["Express", "MongoDB", "Docker","..."] }} />
+                <CardExperience experience={{ date: "June 2024 - June 2025", nameBussines: "Univ. Rosario", roleBussines: "Tutor de Clase", tags: ["Backend","FullStack"] }} />
+                <CardExperience experience={{ date: "Diciembre 2022 - Junio 2023", nameBussines: "SENA", roleBussines: "FullStack Enginer", tags: ["Backend","FullStack"] }} />
+                <CardExperience experience={{ date: "Diciembre 2022 - Junio 2023", nameBussines: "Códifico", roleBussines: "FullStack Enginer", tags: ["Backend","FullStack"] }} />
             </div>
         </section>
     )
